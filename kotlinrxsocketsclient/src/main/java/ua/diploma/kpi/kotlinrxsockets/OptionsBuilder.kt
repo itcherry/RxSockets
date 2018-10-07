59d4395471de004532a84f369b4dd013d9651d24
itcherry <itcherry97@gmail.com>
2018-10-07 22:57:30 +0300

    tests

---
+++ kotlinrxsocketsclient/src/main/java/ua/diploma/kpi/kotlinrxsockets/OptionsBuilder.kt
@@ -0,0 +1,50 @@
+package ua.diploma.kpi.kotlinrxsockets
+
+import io.socket.client.IO
+
+/**
+ * Copyright 2018. Andrii Chernysh
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+/**
+ * Builder for IO.Options for creating pretty DSL
+ *
+ * @author Andrii Chernysh. E-mail: itcherry97@gmail.com
+ *         Developed by <u>Transcendensoft</u>
+ */
+class OptionsBuilder {
+    var mutiplex: Boolean = true
+    var forceNew: Boolean = false
+    var query: String = ""
+    var reconnection: Boolean = true
+    var reconnectionAttempts: Int = 0
+    var reconnectionDelay: Long = 0L
+    var reconnectionDelayMax: Long = 0L
+    var timeout: Long = 20000L
+
+    fun build(): IO.Options {
+        val options = IO.Options()
+        options.multiplex = mutiplex
+        options.forceNew = forceNew
+        options.query = query
+        options.reconnection = reconnection
+        options.reconnectionAttempts = reconnectionAttempts
+        options.reconnectionDelay = reconnectionDelay
+        options.reconnectionDelayMax = reconnectionDelayMax
+        options.timeout = timeout
+
+        return options
+    }
+}
---
+++ kotlinrxsocketsclient/src/main/java/ua/diploma/kpi/kotlinrxsockets/RxSocketBuilder.kt
@@ -0,0 +1,38 @@
+package ua.diploma.kpi.kotlinrxsockets
+
+import io.socket.client.IO
+
+/**
+ * Copyright 2018. Andrii Chernysh
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+/**
+ * Builder for RxSocket for creating pretty DSL
+ *
+ * @author Andrii Chernysh. E-mail: itcherry97@gmail.com
+ *         Developed by <u>Transcendensoft</u>
+ */
+class RxSocketBuilder{
+    var hostIp: String = "PLEASE_SET_HOST_IP_AND_PORT"
+    var port: Int = 0
+    var namespace: String = ""
+    var options: IO.Options? = null
+
+    fun options(block: OptionsBuilder.() -> Unit) {
+        options = OptionsBuilder().apply(block).build()
+    }
+
+    fun build() = RxSocket(hostIp, port, namespace, options)
+}
